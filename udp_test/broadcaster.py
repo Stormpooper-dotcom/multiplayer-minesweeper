53f9cd38d257f8cdedf33d103ad74f98e4c368a5
Stormpooper <lobbt@the-elmgreen-school.org.uk>
2025-12-13 11:20:31 +0000

    Added the UDP test.

---
+++ udp_test/broadcaster.py
@@ -0,0 +1,25 @@
+import socket
+import json
+import time
+import uuid
+
+DISCOVERY_PORT = 37020
+
+game_info = {
+    "type": "beacon",
+    "game_id": str(uuid.uuid4()),
+    "name": "UDP Test",
+    "players": 1,
+    "max_players": 4,
+    "port": 5000
+}
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+print("Broadcasting beacon, press Ctrl+C to stop")
+
+while True:
+    data = json.dumps(game_info).encode("utf-8")
+    sock.sendto("255.255.255.255", DISCOVERY_PORT)
+    time.sleep(1)
---
+++ udp_test/listener.py
@@ -0,0 +1,21 @@
+import socket
+import json
+import time
+
+DISCOVERY_PORT = 37020
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind("", DISCOVERY_PORT)
+
+print("Listening for beacons...")
+
+seen = {}
+
+while True:
+    data, addr = sock.recvfrom(2048)
+    info = json.loads(data.decode("utf-8"))
+
+    ip = addr[0]
+    seen[ip] = time.time()
+
+    print(f"Found game from {ip}: {info}")
